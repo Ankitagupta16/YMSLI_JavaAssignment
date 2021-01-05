e62491238c5995e4542031d058ee518d40c7fb7b
Ankita <1610ankita.gupta@gmail.com>
2021-01-05 22:35:16 +0530

    assignment day 1

---
+++ JavaAssignment/src/day1and2/RightTrianglePattern.java
@@ -0,0 +1,16 @@
+package day1and2;
+
+public class RightTrianglePattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<8;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(j+1+" ");
+			}
+			System.out.println();
+		}
+	}
+}
